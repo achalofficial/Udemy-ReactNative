77fd360559156fe00478c474ede0a062b8c00005
Achal <osndl.achal@aicofindia.com>
2022-09-16 16:53:13 +0530

    Before Event Handling

--- react-refresher/src/App.js
+++ react-refresher/src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
+import GoalList from './components/GoalList';
+import './App.css';
+
 const App = () => {
-  return <h1>A React App!</h1>;
+  const courseGoals = [
+    {id: 'cg1', text: 'Finish the Course'},
+    {id: 'cg2', text: 'Learn all about the Course Main Topic'},
+    {id: 'cg3', text: 'Help other students in the Course Q&A'},
+  ];
+
+  return (
+    <div className="course-goals">
+      <h2>Course Goals</h2>
+      <GoalList goals={courseGoals} />
+    </div>
+  );
 };
 
 export default App;
